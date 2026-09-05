7b0301f769d65e49eef9831821921a90caac8ff9
joahdz <joa.hdz@gmail.com>
2026-09-05 13:11:00 -0500

    Add __init__.py files to aegis package
    Force track all __init__.py files in the aegis package.

---
+++ __init__.py
@@ -0,0 +1,8 @@
+touch aegis/__init__.py
+touch aegis/auditor/__init__.py
+touch aegis/auditor/rules/__init__.py
+touch aegis/defense/__init__.py
+touch aegis/defense/sanitizers/__init__.py
+git add -f aegis/**/__init__.py
+git commit -m "fix: force-track package __init__ files"
+git push origin main
